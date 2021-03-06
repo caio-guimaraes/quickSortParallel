5716847d3f53fbe7412634d196813bf520a14d0d
Caio Guimarães <caio23guimaraes@gmail.com>
2021-03-06 20:27:06 -0300

    refatorando arquivos e trocando dados de int por double

--- quickSort.c
+++ quickSort.c
@@ -1,60 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// PSEUDO CODIGO
-
-void swap(double a1, double a2){
-  double aux = a1;
-  a1 = a2;
-  a2 = aux;
+void imprimiVetor(int vet[], int tamanho){
+    int i;
+    for (i = 0; i < tamanho; i++)
+        printf("%d ", vet[i]);
+    printf("\n");
 }
 
+static int particiona (int vet[], int inicio, int fim) {
+  int pivo = vet[fim]; // pivô
+  int i = inicio;
+  int temp;
+  for (int j = inicio; j < fim; j++){
+    if (vet[j] <= pivo) {
+      temp = vet[i];
+      vet[i] = vet[j];
+      vet[j] = temp;
+      i++;
+    }
+  }
+  temp = vet[i];
+  vet[i] = vet[fim];
+  vet[fim] = temp;
+  return i;
+}
 
-// Algorithm 10.5
-// The sequential Algorithm of Quick Sorting
-void QuickSort(double A[], int i1, int i2) {
-  if ( i1 < i2 ){
-    double pivot = A[i1];
-    int is = i1;
-    double aux;
-    for ( int i = i1+1; i<i2; i++ )
-      if ( A[i] <= pivot ) {
-          is = is + 1;
-          printf("A[is]: %lf A[i]:%lf\n", A[is], A[i]);
-          // swap(A[is], A[i]);
-          aux = A[is];
-          A[is] = A[i];
-          A[i] = aux;
-          printf("Pos Troca\n");
-          printf("A[is]: %lf A[i]:%lf\n\n", A[is], A[i]);
-      }
-    // swap(A[i1], A[is]);
-    aux = A[i1];
-    A[i1] = A[is];
-    A[is] = aux;
-    QuickSort (A, i1, is);
-    QuickSort (A, is+1, i2);
+void quickSort(int vet[], int inicio, int fim){
+  if (inicio < fim){
+    int p = particiona(vet, inicio, fim);
+    quickSort(vet, inicio, p-1);
+    quickSort(vet, p+1, fim);
   }
 }
 
+//vet = 2 5 8 1 6 9 3 0 4 10 7
 int main(int argc, char const *argv[]) {
-  // double[] vetor = [5, 7, 9, 4, 8, 1];
+  int i, tamanho;
 
-  int tamanho, i;
-	printf("Digite o tamanho do vetor: ");
-	scanf("%d", &tamanho);
-	double *vetor = (double*) malloc(tamanho*sizeof(double));
-	printf("Preencha o vetor: ");
-	for(i=0; i<tamanho; i++)
-		scanf("%lf", &vetor[i]);
+  printf("Digite o tamanho do vetor: ");
+  scanf("%d", &tamanho);
 
-  // printf("Vetor original: \n");
-  // imprimiVetor(vetor, tamanho);
+  int *vet = (int*)malloc(tamanho*sizeof(int));
 
-  QuickSort(vetor, 0, tamanho - 1);
+  printf("Preencha o vetor: ");
+  for(i=0; i<tamanho; i++){
+    scanf("%d", &vet[i]);
+  }
 
-  // printf("\nVetor ordenado: \n");
-  // imprimiVetor(vetor, tamanho);
-
+  quickSort(vet, 0, tamanho-1);
+  imprimiVetor(vet, tamanho);
   return 0;
 }
