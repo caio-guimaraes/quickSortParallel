5a9f30266584538b7ad5da11c0c66ea38b801af3
Lucas <Lucas-Amaral-Santos>
2021-03-08 01:08:28 -0300

    Experimentos -- modificação cod QS sequencial

--- quickSort.c
+++ quickSort.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <sys/time.h>
+
+
+void troca(int *vet, int i, int j) {
+  int temp = vet[i];
+  vet[i] = vet[j];
+  vet[j] = temp;
+}
+
 
 void imprimiVetor(int vet[], int tamanho){
     int i;
@@ -9,7 +18,19 @@ void imprimiVetor(int vet[], int tamanho){
     printf("\n");
 }
 
-int particiona (int vet[], int inicio, int fim) {
+int particiona (int *vet, int inicio, int fim) {
+  int i = inicio, pivo = vet[inicio], temp;
+  for (int j = inicio+1; j < fim; j++){
+    if (vet[j] <= pivo) {
+      i++;
+      troca(vet, i, j);
+    }
+  }
+  troca(vet, inicio, i);
+  return i;
+}
+
+/*int particiona (int vet[], int inicio, int fim) {
   int pivo = vet[fim]; // pivô
   int i = inicio;
   int temp;
@@ -25,12 +46,14 @@ int particiona (int vet[], int inicio, int fim) {
   vet[i] = vet[fim];
   vet[fim] = temp;
   return i;
-}
+}*/
 
 void quickSort(int vet[], int inicio, int fim){
   if (inicio < fim){
     int p = particiona(vet, inicio, fim);
-    quickSort(vet, inicio, p-1);
+    // quickSort(vet, inicio, p-1);
+    quickSort(vet, inicio, p);
+
     quickSort(vet, p+1, fim);
   }
 }
@@ -38,7 +61,10 @@ void quickSort(int vet[], int inicio, int fim){
 //vet = 2 5 8 1 6 9 3 0 4 10 7
 int main(int argc, char const *argv[]) {
   int i, tamanho;
-  time_t start, end;
+  double ti, tf;
+
+
+  clock_t t;
 
   printf("Digite o tamanho do vetor: ");
   scanf("%d", &tamanho);
@@ -50,12 +76,14 @@ int main(int argc, char const *argv[]) {
     scanf("%d", &vet[i]);
   }
   // Inicio marcação tempo
-  time(&start);
-  quickSort(vet, 0, tamanho-1);
-  time(&end);
+  t = clock();
+  quickSort(vet, 0, tamanho);
+  t = clock() - t;
   imprimiVetor(vet, tamanho);
+  //printf("Tempo de Execução WMPI: %.4f seconds\n", tf-ti);
 
-  printf("Execution time: %f\n", difftime(end, start));
+  printf("Tempo de execucao: %lf", ((double)t)/((CLOCKS_PER_SEC))); //conversão para double
+  //printf("Execution time: %f\n", difftime(end, start));
   // Fim marcação tempo
   return 0;
 }
